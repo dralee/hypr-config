02fd2520c0f319d41e5cc598b378f86da71adc30
dralee <zhzl_s@126.com>
2024-10-23 10:06:31 +0800

    feat: weather backup

--- .config/waybar/scripts/get_weather.sh
+++ .config/waybar/scripts/get_weather.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # 由于1h更新一次，因此，可以增加重试次数：由原来的5次，3600/2 = 1800,取360次
+# 由于wttr.in经常存在失败情况，因此使用缓存解决 2024.10.23 by dralee
+storefile=$HOME/.config/waybar/scripts/weather.data
+
 for i in {1..360}
 do
     text=$(curl -s "https://wttr.in/$1?format=1")
@@ -11,10 +14,17 @@ do
         then
             tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
             echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
+	    echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}" > $storefile
             exit
         fi
     fi
     sleep 2
 done
-echo "{\"text\":\"error\", \"tooltip\":\"error\"}"
+
+if [[ -f $storefile ]] then
+	cat $storefile
+else
+	#echo $storefile not exists, exit.
+	echo "{\"text\":\"fail\", \"tooltip\":\"error\"}"
+fi
 
