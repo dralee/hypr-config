d0e7b3c959fa7430b7166eed2522974d114e73cd
dralee <zhzl_s@126.com>
2025-07-07 10:41:59 +0800

    poem sync show for waybar

--- .config/waybar/scripts/poem/poem_show.py
+++ .config/waybar/scripts/poem/poem_show.py
@@ -1,6 +1,7 @@
 """
 诗词显示
 2025.1.22 by dralee
+2025.7.7 by dralee monitors sync for show same poem
 """
 import json
 from os.path import getatime,exists,expanduser
@@ -14,6 +15,7 @@ class PoemShow:
         self.db_name ='{}/poem.db'.format(self.root)
         self.db_accessor = PoemDBAccessor(self.db_name)
         self.image_path = '{}/poem.tk'.format(self.root)
+        self.sync = '{}/.sync'.format(self.root)
         self.image_expires_s = 3600 # 1h
 
     def run(self, change_immediately=False):
@@ -23,34 +25,53 @@ class PoemShow:
         #print('change_immediately:',change_immediately)
         #print('exists:',exists(self.root))
         #p = self.db_accessor.get_poem(1)
+        now = int(time.time())
         if not change_immediately and exists(self.image_path):
-            atime = int(getatime(self.image_path))
-            now = int(time.time())
+            atime = self.__last_sync() # int(getatime(self.image_path))
             if now - atime < self.image_expires_s:                
                 p = self.__from_cache()
                 #print('from cache', p)
                 return self.to_waybar_tooltip(p)
         self.db_accessor.reset_poem_record()
         p = self.db_accessor.get_poem_not_show_by_category(11)
-        self.__cache(p)
+        self.__save(self.sync, str(now))
+        self.__cache(p)        
         #print('cache it.')
         #print(p)
         return self.to_waybar_tooltip(p)
+    
+    def __last_sync(self):
+        """上次同步时间"""
+        v = self.__read(self.sync)
+        if v is None:
+            return 0
+        return int(v)
+
+
+    def __save(self, filename, content):
+        """保存"""
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(content)
+
+    def __read(self, filename):
+        """读取"""
+        if not exists(filename):
+            return None
+        with open(filename, 'r', encoding='utf-8') as f:
+            return f.read()
 
     def __cache(self, p):
         """缓存"""
         content = json.dumps(p.to_dict(), ensure_ascii=False)
-        with open(self.image_path, 'w', encoding='utf-8') as f:
-            f.write(content)
+        self.__save(self.image_path, content)
 
     def __from_cache(self):
         """加载"""
-        if exists(self.image_path):
-            with open(self.image_path, 'r', encoding='utf-8') as f:
-                content = f.read()
-                pstr = json.loads(content)
-                return PoemModel.from_dict(pstr)
-        return None
+        content = self.__read(self.image_path)
+        if content is None:
+            return None
+        pstr = json.loads(content)
+        return PoemModel.from_dict(pstr)        
 
     def to_waybar_tooltip(self, p:PoemModel):
         """转换为waybar的tooltip格式"""
