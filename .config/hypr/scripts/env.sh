8fadb34a869d6627da1ceee148f6cb20dee005fb
dralee <zhzl_s@126.com>
2026-01-12 11:25:44 +0800

    feat: env

---
+++ .config/hypr/scripts/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 2026.01.12 by dralee
+# the env lib dependenced by the scripts
+
+# dependenced by weather.py
+pip install pyquery
---
+++ .config/waybar/scripts/env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 2026.01.12 by dralee
+# the env lib dependenced by the scripts
+
+# dependenced by weather.py
+pip install pyquery
+
+# lunar_python by khal.py
+pip install lunar_python 
