85a9526bf36ece737ed5e324d138255960c27377
dralee <zhzl_s@126.com>
2026-04-10 09:55:32 +0800

    feat: monitor

--- .config/hypr/scripts/dbus-monitor/notify.sh
+++ .config/hypr/scripts/dbus-monitor/notify.sh
@@ -2,6 +2,7 @@
 # dbus notify for hyprlock
 # just check the Feish notifiy
 # 2026.2.9 by dralee
+# 2026.4.10 by dralee fix the cut for Chinese show error code
 # $1: Y/y to delete old log
 src_file=$HOME/.config/hypr/scripts/dbus-monitor/Feishu.log
 clear_it=$1
@@ -16,6 +17,9 @@ if [[ "$clear_it" =~ ^[Yy] ]];then
 	exit 0
 fi
 
-line=`head -n 1 $src_file | sed 's#【Feishu】# 󱗆 #g' | cut -c 1-60`
-echo $line
+# cut will cut part of Chinese word for error show
+#line=`head -n 1 $src_file | sed 's#【Feishu】# 󱗆 #g' | cut -c 1-80`
+#echo $line
+line=`head -n 1 $src_file | sed 's#【Feishu】# 󱗆 #g'`
+echo ${line:0:80}
 
