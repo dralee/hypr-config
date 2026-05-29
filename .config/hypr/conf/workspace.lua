956e0b90bbb6a51cb163dad2dfb1f062f739fd78
dralee <zhzl_s@126.com>
2026-05-29 15:38:25 +0800

    feat: workspace

--- .config/hypr/conf/workspace.lua
+++ .config/hypr/conf/workspace.lua
@@ -1,3 +1,8 @@
 -- workspaces
 local name = "default"
+if Hypr_run_env == HYPR_RUN_PC_ENV.HOME then
+    name = "home"
+elseif Hypr_run_env == HYPR_RUN_PC_ENV.LAPTOP then
+    name = "laptop"
+end
 Load_config("workspaces", name)
---
+++ .config/hypr/conf/workspaces/laptop.lua
@@ -0,0 +1,1 @@
+-- workspace for monitors config for laptop
